2441a58cafe5f0ffd2d686aa20d02f77ab3a5a65
sami-colon <abhishek.personal.email@gmail.com>
2020-01-23 16:45:07 +0530

    add-event

--- src/app/add-events/add-events.component.ts
+++ src/app/add-events/add-events.component.ts
@@ -8,8 +8,20 @@ import { Component, OnInit } from '@angular/core';
 export class AddEventsComponent implements OnInit {
 
   constructor() { }
-
+  eventName = 'event Name';
+  eventDate = '';
+  eventAddress = 'Address';
+  eventDescription = 'Description';
+  age = '1-100';
+  cardType = 'Everyone';
+  documentsRequired = 'Documents';
   ngOnInit() {
   }
 
+  onSubmit(nameModel, descModel, addressModel, ageModel, cardTypeModel, docsRequirementsModel, eventDateModel): string {
+    // nameModel, descModel, addressModel, ageModel, cardTypeModel, docsRequirementsModel, eventDateModel
+    // tslint:disable-next-line:max-line-length
+    console.log(nameModel.value, descModel.value, addressModel.value, ageModel.value, cardTypeModel.value, docsRequirementsModel.value, eventDateModel.value);
+    return '';
+  }
 }
